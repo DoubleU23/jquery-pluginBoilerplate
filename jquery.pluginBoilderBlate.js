ddc66246c7da61a2cb5c459b152887c741221290
Stefan Friedl <stefan.friedl@wunderman.com>
2014-01-09 13:06:16 +0100

    changed idention to 2 spaces

--- jquery.pluginBoilderBlate.js
+++ jquery.pluginBoilderBlate.js
@@ -1,73 +1,73 @@
 ;(function(window, $, undefined) {
-	"use strict";
+  "use strict";
 
-	$.pluginName	= $.pluginName || function(_el, _settings) {
-		var _this			= this,
-			_$el			= $(_el),
-			_privateVar		= 'X',
+  $.pluginName    = $.pluginName || function(_el, _settings) {
+    var _this           = this,
+      _$el            = $(_el),
+      _privateVar     = 'X',
 
-		_init				= function() {
-			// do initialisation
-		},
-		_getPrivateVar		= function(){
-			return _x;
-		};
+    _init               = function() {
+      // do initialisation
+    },
+    _getPrivateVar      = function(){
+      return _x;
+    };
 
-		/***************************************************************
-		*****                                           ****************
-		*****            PUBLIC VARS                    ****************
-		*****                                           ****************
-		***************************************************************/
-		this.publicVar		= _privateVar;
+    /***************************************************************
+    *****                                           ****************
+    *****            PUBLIC VARS                    ****************
+    *****                                           ****************
+    ***************************************************************/
+    this.publicVar      = _privateVar;
 
-		/***************************************************************
-		*****                                           ****************
-		*****            PUBLIC METHODS                 ****************
-		*****                                           ****************
-		***************************************************************/
-		this.publicMethod		= function() {
-			return _getPrivateVar();
-		};
+    /***************************************************************
+    *****                                           ****************
+    *****            PUBLIC METHODS                 ****************
+    *****                                           ****************
+    ***************************************************************/
+    this.publicMethod       = function() {
+      return _getPrivateVar();
+    };
 
-		this.update				= function() {
-			// do update
-		};
+    this.update             = function() {
+      // do update
+    };
 
-	};
+  };
 
-	$.pluginName.defaults   = {
+  $.pluginName.defaults   = {
 
-	};
+  };
 
-	$.fn.pluginName   = function(options) {
-		var args    = Array.prototype.slice.call( arguments, 1 ),
-			returnVal;
+  $.fn.pluginName   = function(options) {
+    var args    = Array.prototype.slice.call( arguments, 1 ),
+      returnVal;
 
-		this.each(function(){
-			var $form       = $(this),
-				instance    = $form.data('pluginName');
-			if( typeof options === 'string' ){
-				if( instance ){
-					if( typeof instance[options] === 'function' ){
-						returnVal = instance[options].apply( instance, args);
-					}else{
-						$.error('[pluginName] method "'+options+'" not available');
-					}
-				}else{
-					$.error('[pluginName] cannot call methods prior to initialization');
-				}
-			}else if( typeof options === 'object' || !options ){
-				var settings = $.extend(true, {}, $.pluginName.defaults, options);
-				if( instance ){
-					instance.init(options)
-				}else{
-					$form.data('pluginName', new $.pluginName(this, settings));
-				}
-			}
-		});
-		if( returnVal !== undefined ){ return returnVal; }
-		return this;
-	};
+    this.each(function(){
+      var $form       = $(this),
+        instance    = $form.data('pluginName');
+      if( typeof options === 'string' ){
+        if( instance ){
+          if( typeof instance[options] === 'function' ){
+            returnVal = instance[options].apply( instance, args);
+          }else{
+            $.error('[pluginName] method "'+options+'" not available');
+          }
+        }else{
+          $.error('[pluginName] cannot call methods prior to initialization');
+        }
+      }else if( typeof options === 'object' || !options ){
+        var settings = $.extend(true, {}, $.pluginName.defaults, options);
+        if( instance ){
+          instance.init(options)
+        }else{
+          $form.data('pluginName', new $.pluginName(this, settings));
+        }
+      }
+    });
+    if( returnVal !== undefined ){ return returnVal; }
+    return this;
+  };
 
 
 })(window, window.jQuery);
